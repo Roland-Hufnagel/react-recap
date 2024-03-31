fce0bbb575d2fa15aa85c970b6d3de2fdd00dd0c
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-03-31 15:43:22 +0200

    part 5

--- src/App.js
+++ src/App.js
@@ -2,24 +2,49 @@ import "./App.css";
 import { themes as initialThemes } from "./db";
 import Theme from "./components/Theme";
 import ThemeForm from "./components/ThemeForm";
-import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import useLocalStorageState from "use-local-storage-state";
 //import uuid from "uuid";
 
 function App() {
-  const [themes, setThemes] = useState(initialThemes);
-  console.log(themes);
-  function handleAddTheme(theme) {
+  const [themes, setThemes] = useLocalStorageState("themes", {
+    defaultValue: initialThemes,
+  });
+  async function getColorName(hex) {
+    const response = await fetch(
+      `https://www.thecolorapi.com/id?hex=${hex.slice(1)}`
+    );
+    const data = await response.json();
+    return data.name.value;
+  }
+  async function handleAddTheme(theme) {
     const newTheme = {};
     newTheme.id = uuidv4();
     //newTheme.id = uuid.v4();
     newTheme.name = theme.name;
     newTheme.colors = [
-      { role: "primary", value: theme.color1 },
-      { role: "secondary", value: theme.color2 },
-      { role: "surface", value: theme.color3 },
-      { role: "surface-on", value: theme.color4 },
+      {
+        role: "primary",
+        value: theme.color1,
+        name: await getColorName(theme.color1),
+      },
+      {
+        role: "secondary",
+        value: theme.color2,
+        name: await getColorName(theme.color2),
+      },
+      {
+        role: "surface",
+        value: theme.color3,
+        name: await getColorName(theme.color3),
+      },
+      {
+        role: "surface-on",
+        value: theme.color4,
+        name: await getColorName(theme.color4),
+      },
     ];
+    newTheme.colors.forEach((color) => {});
     setThemes([newTheme, ...themes]);
   }
   function handleDeleteTheme(id) {
@@ -31,7 +56,11 @@ function App() {
       <h1>Theme Creator</h1>
       <ThemeForm handleAddTheme={handleAddTheme} />
       {themes.map((theme) => (
-        <Theme key={theme.id} theme={theme} handleDeleteTheme={handleDeleteTheme} />
+        <Theme
+          key={theme.id}
+          theme={theme}
+          handleDeleteTheme={handleDeleteTheme}
+        />
       ))}
     </main>
   );
--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -1,14 +1,31 @@
+//import { useEffect, useState } from "react";
 import "./ColorCard.css";
 
 export default function ColorCard({ color }) {
+  // const [colorName, setColorName] = useState("");
+  // useEffect(() => {
+  //   async function fetchColorName() {
+  //     const response = await fetch(
+  //       `https://www.thecolorapi.com/id?hex=${color.value.slice(1)}`
+  //     );
+  //     const data = await response.json();
+  //     setColorName(data.name.value);
+  //   }
+  //   fetchColorName();
+  // }, []);
   return (
     <>
       <section className="card">
         <div className="card--info">
           <h2>{color.role}</h2>
           <p>{color.value}</p>
+          {/* <p>{colorName}</p> */}
+          <p>{color.name}</p>
         </div>
-        <div className="color" style={{backgroundColor: `${color.value}`}}></div>
+        <div
+          className="color"
+          style={{ backgroundColor: `${color.value}` }}
+        ></div>
       </section>
     </>
   );
